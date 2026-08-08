8ae97b699b73f8158db87e274653796e07aea79b
h3rbass <gamerstyle256@gmail.com>
2026-08-08 18:13:19 -0400

    PWA Feature

---
+++ wwwroot/service-worker.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "sgimadsi-v2";
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.add(self.registration.scope))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    const url = new URL(event.request.url);
+
+    // No interceptar peticiones externas
+    if (url.origin !== self.location.origin) {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200) {
+                            return response;
+                        }
+
+                        const responseClone = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseClone);
+                            });
+
+                        return response;
+                    });
+            })
+            .catch(() => caches.match(self.registration.scope))
+    );
+});
